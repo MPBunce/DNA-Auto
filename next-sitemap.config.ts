4c66c531032ba412677e0b81adc6398e30863d7a
MPBunce <matthew-bunce@outlook.com>
2026-03-22 14:55:49 -0400

    rename

--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.dnaautosource.com/',  // replace with your domain
-  generateRobotsTxt: true,            // generates robots.txt too
-  // optional extras:
-  changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 5000,
-}
