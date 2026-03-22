600bc7bc5e11d8f3fd97d7bc776a2513fd60e732
MPBunce <matthew-bunce@outlook.com>
2026-03-22 14:51:03 -0400

    rebnase

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.dnaautosource.com/',  // replace with your domain
+  generateRobotsTxt: true,            // generates robots.txt too
+  // optional extras:
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
